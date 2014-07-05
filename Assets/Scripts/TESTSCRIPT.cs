4d6056eca53d0e6925d0233728bb83faf77035f7
Alex <jonshaft@web.de>
2014-07-05 02:57:38 +0200

    Testing with Scripts and Prefabs

---
+++ Assets/Scripts/TESTSCRIPT.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTSCRIPT : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        this.transform.Rotate(1, 2, 3);
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
